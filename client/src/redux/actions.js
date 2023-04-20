b41b4be3f7c1c8f7b469017f72dd7ae536600b61
franklyn castro <franklyncastro@hotmail.com>
2023-04-20 16:49:23 -0400

    Landing funcionando

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -3,7 +3,7 @@ import { GET_ALL_GAMES } from "./actions-types";
 export const getAllVideogames = () => {
   return async (dispatch) => {
     try {
-      await fetch("http://localhost:3002/videogames")
+      await fetch("http://localhost:3001/videogames")
         .then((res) => res.json())
         .then((payload) => {
           dispatch({ type: GET_ALL_GAMES, payload });
